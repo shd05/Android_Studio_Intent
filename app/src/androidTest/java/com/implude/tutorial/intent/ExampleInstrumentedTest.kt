5191cc1543e9c27249a005794c0349007bf7b868
nutyworks <nutyworks@gmail.com>
2021-06-09 00:09:56 +0900

    test: add open another activity test

--- app/src/androidTest/java/com/implude/tutorial/intent/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/implude/tutorial/intent/ExampleInstrumentedTest.kt
@@ -1,12 +1,20 @@
 package com.implude.tutorial.intent
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +23,30 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val rule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun setup() {
+        Intents.init()
+    }
+
+    @After
+    fun tearDown() {
+        Intents.release()
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.implude.tutorial.intent", appContext.packageName)
+    }
+
+    @Test
+    fun clickOpenBtn_opensAnotherActivity() {
+        onView(withId(R.id.btn_open)).perform(click())
+        intended(hasComponent(AnotherActivity::class.java.name))
     }
 }
