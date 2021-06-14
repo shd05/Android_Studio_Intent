25ddacce890f2d79e13e937e1a934b1408b709be
shd05 <shd05@naver.com>
2021-06-14 21:27:51 +0900

    .

--- app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
+++ app/src/main/java/com/implude/tutorial/intent/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.implude.tutorial.intent
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_open=findViewById<Button>(R.id.btn_open)
+
+        btn_open.setOnClickListener{
+           val Intent = (Intent(this, Sub::class.java))
+            startActivity(Intent)
+        }
     }
+
 }
---
+++ app/src/main/java/com/implude/tutorial/intent/Sub.java
@@ -0,0 +1,14 @@
+package com.implude.tutorial.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sub extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub2);
+    }
+}
